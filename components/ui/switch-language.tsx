f1aa859ec3444988675ff54961a7c947f42a5858
Aynaz Heydariafshar <aynaz@arianalabs.io>
2024-09-23 22:44:55 +0330

    FEAT : add ui for select of language

--- components/ui/switch-language.tsx
+++ components/ui/switch-language.tsx
@@ -3,6 +3,14 @@
 import { useLocale } from "next-intl";
 import { usePathname, useRouter } from "next/navigation";
 import { ChangeEvent } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./select";
 
 export default function SwitchLanguage() {
   const currentLocale = useLocale();
@@ -10,19 +18,28 @@ export default function SwitchLanguage() {
   const pathname = usePathname();
   const cleanPathname = pathname.replace(/^\/(en|fa)(\/|$)/, "/");
 
-  function handleChangeLang(event: ChangeEvent<HTMLSelectElement>) {
-    const nextLocale = event.target.value;
+  function handleChangeLang(value: ChangeEvent<HTMLSelectElement>) {
     if (pathname.startsWith("/fa") || pathname.startsWith("/en")) {
-      router.replace(`/${nextLocale}${cleanPathname}`);
+      router.replace(`/${value}${cleanPathname}`);
     } else {
-      router.replace(`${nextLocale}`);
+      router.replace(`${value}`);
     }
   }
 
   return (
-    <select onChange={(e) => handleChangeLang(e)} defaultValue={currentLocale}>
-      <option value="en">en</option>
-      <option value="fa">fa</option>
-    </select>
+    <Select
+      onValueChange={(value) => handleChangeLang(value)}
+      defaultValue={currentLocale}
+    >
+      <SelectTrigger className="bg-transparent w-20 border-none ring-0 focus:ring-0">
+        <SelectValue placeholder="Select a timezone" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectItem value="en">en</SelectItem>
+          <SelectItem value="fa">fa</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
   );
 }
