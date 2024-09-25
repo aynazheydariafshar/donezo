05c583f9c4778c632fffea1922ef312349b20d5a
Aynaz Heydariafshar <aynaz@arianalabs.io>
2024-09-25 15:58:02 +0330

    FEAT : change color of buttons on navbar

--- app/[locale]/(homePage)/_components/navbar.tsx
+++ app/[locale]/(homePage)/_components/navbar.tsx
@@ -3,18 +3,24 @@ import Logo from "@/components/logo";
 import SwitchLanguage from "@/components/switch-language";
 import SwitchTheme from "@/components/switch-theme";
 import { Button } from "@/components/ui/button";
-import { useTranslations } from "next-intl";
+import { useLocale, useTranslations } from "next-intl";
+import Link from "next/link";
 
 export default function NavbarHomePage() {
   const t = useTranslations();
+  const local = useLocale();
   return (
     <div className="fixed top-0 w-full  flex items-center justify-between px-11 h-16 shadow-sm bg-black black:bg-white bg-opacity-35 z-50">
       <div>
         <Logo />
       </div>
       <div className="flex items-center space-x-3">
-        <Button>{t("login")}</Button>
-        <Button>{t("go-to-your-boards")}</Button>
+        <Button size="sm" asChild>
+          <Link href={`/${local}/sign-in`}>{t("login")}</Link>
+        </Button>
+        <Button variant="secondary" size="sm" asChild>
+          <Link href="/">{t("go-to-your-boards")}</Link>
+        </Button>
         <SwitchTheme />
         <SwitchLanguage />
       </div>
