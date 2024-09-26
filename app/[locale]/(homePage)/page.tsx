1b6996e4a2a0a2b06edb3c8ee8bc4a7faf051998
Aynaz Heydariafshar <aynaz@arianalabs.io>
2024-09-26 20:01:04 +0330

    FEAT : add border ui for text

--- app/[locale]/(homePage)/page.tsx
+++ app/[locale]/(homePage)/page.tsx
@@ -16,16 +16,20 @@ function HomePage() {
       <div className="fixed top-0 left-0 transform rotate-[80deg]">
         <div className="absolute opacity-10 w-wave h-wave ml-wave-left mt-wave-top rounded-wave animate-rotate-fast bg-gray-800 dark:bg-white"></div>
       </div>
-      <div className="flex justify-center items-center pt-40 px-11 z-10 relative">
+      <div className="flex justify-center items-center pt-20 md:pt-32 px-11 z-10 relative">
         <div className="flex flex-col justify-center items-center space-y-4">
-          <h1 className="text-6xl">{t("welcome-to-donezo")}</h1>
-          <h2 className="text-2xl p-2  rounded-lg">{t("donezo-Slogan")}</h2>
-          <p className="text-xl text-center max-w-[65vw]">
+          <h1 className="text-3xl md:text-6xl text-center">
+            {t("welcome-to-donezo")}
+          </h1>
+          <h2 className="text-lg md:text-2xl py-1 px-3 text-center rounded-lg bg-orange-500 border border-black text-black transition [box-shadow:rgb(171,_196,245)-8px_8px] hover:[box-shadow:rgb(171,_196,_245)0px_0px]">
+            {t("donezo-Slogan")}
+          </h2>
+          <p className="text-lg md:text-2xl text-center max-w-[65vw]">
             {t("donezo-content-home-page")}
           </p>
           <Button
             size="lg"
-            className="bg-orange-400 dark:bg-orange-500 text-xl border border-black text-black transition [box-shadow:rgb(171,_196,245)-8px_8px] hover:[box-shadow:rgb(171,_196,_245)0px_0px] hover:bg-pink-400"
+            className="bg-orange-500 text-xl border border-black text-black transition [box-shadow:rgb(171,_196,245)-8px_8px] hover:[box-shadow:rgb(171,_196,_245)0px_0px] hover:bg-orange-600"
           >
             <Link className="font-bold" href={`/${locale}/sign-up`}>
               {t("get-started")}
