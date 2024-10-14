05c2b914ed0a52c6c212d43a45e9cf48b815f479
Aynaz Heydariafshar <aynaz@arianalabs.io>
2024-10-14 09:35:19 +0330

    Remove .next from tracking

--- .next/static/chunks/app/(platform)/protected/page-fd6d501b292f5d4c.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[617],{7110:function(n,e,u){Promise.resolve().then(u.bind(u,63))},63:function(n,e,u){"use strict";u.r(e),u.d(e,{default:function(){return i}});var t=u(6764),r=u(7787);function i(){return(0,t.jsx)("div",{children:(0,t.jsx)(r.l8,{})})}}},function(n){n.O(0,[787,677,14,744],function(){return n(n.s=7110)}),_N_E=n.O()}]);
